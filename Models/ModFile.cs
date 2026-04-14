6bfe5a3f191154fe4495fa3fed6859d2f3f0a9d5
Slineham29 <samuel.lineham@hotmail.com>
2026-04-14 16:33:01 +0100

    Created a new class to contain the information for each mod, and a new library to better parse the AddonInfo.txt file.

---
+++ Models/ModFile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace l4d2_mod_manager.Models;
+
+public class ModFile
+{
+    public string ModName { get; set; } = string.Empty;
+    public string FileName { get; set; } = string.Empty;
+    public string ModDescription  { get; set; }  = string.Empty;
+    public string ModAuthor  { get; set; }  = string.Empty;
+    public bool IsMap  { get; set; }
+    
+    public List<string> FileList = [];
+    public string ImageLink { get; set; } = string.Empty;
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using SteamDatabase.ValvePak;
 using Avalonia.Platform.Storage;
+using l4d2_mod_manager.Models;
+using ValveKeyValue;
 
 namespace l4d2_mod_manager.ViewModels;
 
@@ -15,6 +18,8 @@ public partial class MainWindowViewModel : ViewModelBase
 {
     [ObservableProperty]
     private string _gameDirectory = "N/A";
+
+    [ObservableProperty] private ObservableCollection<ModFile> _modCollection = new();
 
     [ObservableProperty]
     private ObservableCollection<string> _conflictList = new();
@@ -57,31 +62,111 @@ public partial class MainWindowViewModel : ViewModelBase
         }
     }
 
+    private ModFile CreateNewModFile(Package package, PackageEntry addonInfo)
+    {
+        package.ReadEntry(addonInfo, out byte[] data);
+        var ms = new MemoryStream(data);
+        var kvs = KVSerializer.Create(KVSerializationFormat.KeyValues1Text);
+        KVObject convertedInfo;
+        try
+        {
+            convertedInfo = kvs.Deserialize(ms);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            convertedInfo = null;
+        }
+
+        ModFile mod = new ModFile();
+        
+        if (convertedInfo != null)
+        {
+            foreach (var child in convertedInfo.Children)
+            {
+                string key = child.Key;
+                switch (key.ToLower())
+                {
+                    case "addontitle":
+                        mod.ModName = child.Value.ToString();
+                        break;
+                    case "addondescription":
+                        mod.ModDescription = child.Value.ToString();
+                        break;
+                    case "addonauthor":
+                        mod.ModAuthor = child.Value.ToString();
+                        break;
+                    case "addoncontent_campaign":
+                    case "addoncontent_map":
+                        if (mod.IsMap == false)
+                        {
+                            mod.IsMap = String.Equals(child.Value.ToString(), "1");
+                        }
+                        break;
+                }
+            }
+        }
+        else
+        {
+            mod.FileName = "Corrupt AddonInfo";
+        }
+        return mod;
+    }
+
     private Dictionary<string, List<string>> ParseVpkFiles()
     {
         var vpkFiles = Directory.EnumerateFiles(GameDirectory, "*.vpk", SearchOption.AllDirectories);
-
         var conflictMapper = new Dictionary<string, List<string>>();
 
         foreach (var vpkFile in vpkFiles)
         {
+            Console.WriteLine($"Reading {Path.GetFileName(vpkFile)}");
+            ModFile currentMod;            
             using var package = new Package();
             package.Read(vpkFile);
-            string packageName = Path.GetFileName(vpkFile);
+            var infoFile = package.FindEntry("addoninfo.txt");
+            if (infoFile != null)
+            {
+                currentMod = CreateNewModFile(package, infoFile);
+                currentMod.FileName = Path.GetFileName(vpkFile);
+            }
+            else
+            {
+                currentMod = new ModFile
+                {
+                    ModName = "Invalid Mod Info",
+                    FileName = Path.GetFileName(vpkFile)
+                };
+            }
+            string imageLocation = Path.GetDirectoryName(vpkFile) + @"\" + Path.GetFileNameWithoutExtension(vpkFile) + ".jpg";
+            if (Directory.Exists(imageLocation))
+            {
+                currentMod.ImageLink = imageLocation;
+            }
+            
+            if (currentMod.IsMap)
+            {
+                continue;
+            }
 
             foreach (var fileType in package.Entries)
             {
                 foreach (var entry in fileType.Value)
                 {
+                    if(entry.TypeName == "txt" || entry.TypeName == "jpg" || entry.TypeName == "cache")
+                    {
+                        continue;
+                    }
                     string filePath = $"{entry.DirectoryName}/{entry.FileName}.{entry.TypeName}";
                     if (!conflictMapper.ContainsKey(filePath))
                     {
                         conflictMapper[filePath] = new List<string>();
                     }
-
-                    conflictMapper[filePath].Add(packageName);
+                    conflictMapper[filePath].Add(currentMod.ModName);
+                    currentMod.FileList.Add(filePath);
                 }
             }
+            ModCollection.Add(currentMod);
         }
         return conflictMapper;
     }
